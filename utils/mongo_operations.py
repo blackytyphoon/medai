b4a78e111da29d9eb76a1de54118b4af38d6d1b5
Shivam Tripathi <119807222+blackytyphoon@users.noreply.github.com>
2024-11-22 00:29:12 +0530

    Add files via upload

---
+++ utils/mongo_operations.py
@@ -0,0 +1,26 @@
+import pymongo
+import hashlib
+
+class MongoDBOperations:
+    def __init__(self, uri):
+        self.client = pymongo.MongoClient(uri)
+        self.db = self.client["med_ai_database"]
+        self.collection = self.db["users"]
+
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def signup(self, name, email, password):
+        hashed_password = self.hash_password(password)
+        if self.collection.find_one({"email": email}):
+            return False
+        self.collection.insert_one({"name": name, "email": email, "password": hashed_password})
+        return True
+
+    def login(self, email, password):
+        hashed_password = self.hash_password(password)
+        user = self.collection.find_one({"email": email, "password": hashed_password})
+        return user is not None
+
+    def close_connection(self):
+        self.client.close()
